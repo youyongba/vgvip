df4a42a085054bf7cac0ddbe1591cd539ab598c7
xiaohong <55377146@qq.com>
2026-03-03 06:38:05 +0800

    PC -- 指挥舱一级页 -- bug -- 建设进度 左侧列表问题 【已改】

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const routes = require('./routes');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,13 +1,11 @@
 const { Router } = require('express');
-const path = require('path');
 
 const router = Router();
-const publicDir = path.join(__dirname, '../../public');
 
-router.get('/', (_req, res) => res.sendFile(path.join(publicDir, 'vip.html')));
-router.get('/vip', (_req, res) => res.sendFile(path.join(publicDir, 'vip.html')));
-router.get('/benefits', (_req, res) => res.sendFile(path.join(publicDir, 'benefits.html')));
-router.get('/faq', (_req, res) => res.sendFile(path.join(publicDir, 'faq.html')));
-router.get('/contact', (_req, res) => res.sendFile(path.join(publicDir, 'contact.html')));
+router.get('/', (_req, res) => res.render('pages/vip'));
+router.get('/vip', (_req, res) => res.render('pages/vip'));
+router.get('/benefits', (_req, res) => res.render('pages/benefits'));
+router.get('/faq', (_req, res) => res.render('pages/faq'));
+router.get('/contact', (_req, res) => res.render('pages/contact'));
 
 module.exports = router;
